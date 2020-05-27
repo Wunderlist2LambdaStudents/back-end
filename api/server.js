ac220b95e0b56f13bc771b9b078e1ba3bd5fc245
aurelio4 <aurelioarcabascio@gmail.com>
2020-05-27 11:36:12 -0400

    added an api test at root url

--- api/server.js
+++ api/server.js
@@ -7,6 +7,11 @@ const server = express()
 const PORT = process.env.PORT || 4000
 
 server.use(express.json())
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'working' })
+})
+
 server.use('/auth', authRouter)
 server.use('/todo', todoRouter)
 
